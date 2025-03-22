891b3d417de91fff523d5d975ee23b3e0ccf7294
maclux <tim@geohei.lu>
2025-03-22 22:33:52 +0100

    feat: implement command handling

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,71 @@
-import { Client, Events, GatewayIntentBits } from "discord.js"
+import {
+  Client,
+  Collection,
+  Events,
+  GatewayIntentBits,
+  MessageFlags,
+} from "discord.js";
+import fs from "fs";
+import path from "path";
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.once(Events.ClientReady, readyClient => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+client.commands = new Collection();
+
+const foldersPath = path.join(process.cwd(), "src", "commands");
+const commandFolders = fs.readdirSync(foldersPath);
+
+(async () => {
+  for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs
+      .readdirSync(commandsPath)
+      .filter((file) => file.endsWith(".ts"));
+    for (const file of commandFiles) {
+      const filePath = path.join(commandsPath, file);
+      const command = (await import(filePath)).default;
+
+      if ("data" in command && "execute" in command) {
+        client.commands.set(command.data.name, command);
+      } else {
+        console.log(
+          `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+        );
+      }
+    }
+  }
+})();
+
+client.once(Events.ClientReady, (readyClient) => {
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: "There was an error while executing this command!",
+        flags: MessageFlags.Ephemeral,
+      });
+    } else {
+      await interaction.reply({
+        content: "There was an error while executing this command!",
+        flags: MessageFlags.Ephemeral,
+      });
+    }
+  }
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN);
---
+++ src/types/discordjs.d.ts
@@ -0,0 +1,8 @@
+import "discord.js";
+import { Collection, SlashCommandBuilder } from "discord.js";
+
+declare module "discord.js" {
+  interface Client {
+    commands: Collection<string, { data: SlashCommandBuilder, execute: (interaction: ChatInputCommandInteraction) => Promise<void> }>;
+  }
+}
